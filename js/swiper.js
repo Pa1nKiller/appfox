892404b3f7a16f72fc94d79652c8443a17d6b5a5
Pa1nKiller <85261352+Pa1nKiller@users.noreply.github.com>
2023-06-16 16:31:26 +0300

    Add files via upload

--- js/swiper.js
+++ js/swiper.js
@@ -1,1 +1,11 @@
-new Swiper('.SW')
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
