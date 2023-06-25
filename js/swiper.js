8d34233aa44030940aedfb7d7c750b0c69780843
Pa1nKiller <85261352+Pa1nKiller@users.noreply.github.com>
2023-06-25 21:10:12 +0300

    adaptive swiper

--- js/swiper.js
+++ js/swiper.js
@@ -2,21 +2,33 @@ const mySwiper = new Swiper('.kitchen-swiper', {
   direction: 'horizontal',
   loop: false,
   speed: 560, 
-  slidesPerColumn: 1,
-  slidesPerView: 1,
+  slidesPerColumn: 2,
+  slidesPerView: 2,
   slidesPerGroup: 1,
-  spaceBetween: 34,
+  spaceBetween: 10,
   grid: {
-    rows: 3,
+    rows: 2,
     fill: 'row',
   },
 
   breakpoints: {
+    360: {
+      slidesPerColumn: 2,
+      slidesPerView: 2,
+      slidesPerGroup: 1,
+      spaceBetween: 20,
+      grid: {
+        rows: 2,
+        fill: 'row',
+      },
+    },
+
+
     439: {
       slidesPerColumn: 2,
       slidesPerView: 2,
       slidesPerGroup: 2,
-      spaceBetween: 54,
+      spaceBetween: 40,
       grid: {
         rows: 4,
         fill: 'row',
