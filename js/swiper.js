13a5132d87976c8e13d8212758e67ff87631e2bc
Pa1nKiller <85261352+Pa1nKiller@users.noreply.github.com>
2023-06-25 17:02:54 +0300

    swiper

--- js/swiper.js
+++ js/swiper.js
@@ -1,12 +1,31 @@
-const swiper = new Swiper('.kitchen-swiper', {
-    // Optional parameters
-    loop: true,
-    simulateTouch: false,
-    autoHeight: true,
+const mySwiper = new Swiper('.kitchen-swiper', {
+  direction: 'horizontal',
+  loop: false,
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+  slidesPerColumn: 1,
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  spaceBetween: 20,
+  grid: {
+    rows: 4,
+    fill: 'row',
+  },
+
+  breakpoints: {
+    439: {
+      slidesPerColumn: 2,
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      spaceBetween: 54,
+      grid: {
+        rows: 2,
+        fill: 'row',
+      },
     },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
+
